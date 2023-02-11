ca4cf9c9e7cf2b9c8a407f76c396a14b6dd03c45
ShoKubota <sh2o.1114@mineo.jp>
2023-02-11 22:09:42 +0900

    Headline.jsを作る

---
+++ components/Headline.js
@@ -0,0 +1,36 @@
+import Image from 'next/image'
+import { Inter, Tiro_Telugu } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Headline(props) {
+  return (
+    <div>
+      <div className={styles.description}>
+        <h1>{props.title}</h1>
+        <p>
+          Get started by editing&nbsp;
+          <code className={styles.code}>pages/index.js</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            By{' '}
+            <Image
+              src="/vercel.svg"
+              alt="Vercel Logo"
+              className={styles.vercelLogo}
+              width={100}
+              height={24}
+              priority
+            />
+          </a>
+        </div>
+      </div>
+    </div>
+  )
+}
