a669fd61ce367cc0b2067b278dd60841ab280625
ShoKubota <sh2o.1114@mineo.jp>
2023-02-14 17:45:43 +0900

    Mainコンポーネントに書き換える

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
 import Head from 'next/head'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-import { Links } from '@/components/Links'
-import { Headline } from '@/components/Headline'
+import { Main } from '@/components/Main'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,10 +14,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <Headline title="About Page"/>
-        <Links />
-      </main>
+      <Main 
+        title={"about"}
+      />
     </>
   )
 }
